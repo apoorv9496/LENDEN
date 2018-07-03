836cc21948933957392b3967635ae65cbf2a3a11
Apoorv Jain <apoorv9496@gmail.com>
2018-07-04 04:10:32 +0530

    request struct and lists

--- lenden.sol
+++ lenden.sol
@@ -5,6 +5,9 @@ contract lenden {
     // owner address
     address _owner;
     
+    // accounting total users + acting as unique ID
+    uint _userCounter = 0;
+    
     // verify owner
     modifier owner {
         require(msg.sender == _owner);
@@ -12,6 +15,7 @@ contract lenden {
     }
     
     struct user{
+        uint id;
         string name;
         bool active;
         uint rating;
@@ -19,14 +23,35 @@ contract lenden {
         uint borrows;
     }
     
+    struct request {
+        address reqAddress;
+        uint interestRate;
+        uint period;
+        bool active;
+    }
+    
     // mapping address to a person info
     mapping (address => user) public _userList;
+    
+    request[] _borrowRequestList;
+    request[] _lendRequestList;
     
     constructor() public {
         _owner = msg.sender;
     }
     
-    function createAccount() public owner{
+    // owner calls to create an account only after KYC success of a to be user
+    function createAccount(string name, address userAddress) public owner {
+        _userCounter++;
+        
+        _userList[userAddress].name = name;
+        _userList[userAddress].active = true;
+        _userList[userAddress].rating = 0;
+        _userList[userAddress].lends = 0;
+        _userList[userAddress].borrows = 0;
+    }
+    
+    function createBorrowRequest() public {
         
     }
 }
