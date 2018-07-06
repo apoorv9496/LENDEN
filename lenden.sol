8fe7bef2070f5b6aa1f30bf3522f5f13823ca07c
Apoorv Jain <apoorv9496@gmail.com>
2018-07-07 03:13:11 +0530

    updated lend function

--- lenden.sol
+++ lenden.sol
@@ -25,16 +25,28 @@ contract lenden {
     
     struct request {
         address reqAddress;
+        uint amount;
+        uint interestRate;
+        uint period;
+        bool active;
+    }
+    
+    struct pending {
+        address from;
+        address to;
+        uint amount;
         uint interestRate;
         uint period;
         bool active;
     }
     
     // mapping address to a person info
-    mapping (address => user) public _userList;
+    mapping (address => user) private _userList;
     
-    request[] _borrowRequestList;
-    request[] _lendRequestList;
+    request[] public _borrowRequestList;
+    request[] public _lendRequestList;
+    
+    pending[] private _pendingList;
     
     constructor() public {
         _owner = msg.sender;
@@ -51,7 +63,74 @@ contract lenden {
         _userList[userAddress].borrows = 0;
     }
     
-    function createBorrowRequest() public {
+    // create and adding a request to the list
+    function createRequest(uint amount, uint rate, uint period, string reqType) public returns (string) {
         
+        request storage temp;
+        temp.reqAddress = msg.sender;
+        temp.amount = amount;
+        temp.interestRate = rate;
+        temp.period = period;
+        temp.active = true;
+        
+        if(keccak256(bytes(reqType)) == keccak256("borrow")) {
+            _borrowRequestList.push(temp);    
+        }
+        else {
+            _lendRequestList.push(temp);    
+        }
+        
+        return "request was registered successfully";
     }
+    
+    // function to lend moeny to a person
+    function lend(uint index) public payable returns (string) {
+        if(_borrowRequestList[index].active) {
+            _borrowRequestList[index].active = false;
+        
+            pending storage temp;
+            temp.from = msg.sender;
+            temp.to = _borrowRequestList[index].reqAddress;
+            temp.amount = _borrowRequestList[index].amount;
+            temp.interestRate = _borrowRequestList[index].interestRate;
+            temp.period = _borrowRequestList[index].period;
+            temp.active = true; 
+            
+            _pendingList.push(temp);
+        }
+        
+        return "requests not active";
+    }
+    
+    // function to lend moeny to a person
+    function payBack(uint index) public payable returns (string) {
+        if(_borrowRequestList[index].active) {
+            _borrowRequestList[index].active = false;
+        
+            pending storage temp;
+            temp.from = msg.sender;
+            temp.to = _borrowRequestList[index].reqAddress;
+            temp.amount = _borrowRequestList[index].amount;
+            temp.interestRate = _borrowRequestList[index].interestRate;
+            temp.period = _borrowRequestList[index].period;
+            temp.active = true; 
+            
+            _pendingList.push(temp);
+        }
+        
+        return "requests not active";
+    }
+    
+    /*
+    function getBorrowList() public constant returns (request[]) {
+        // send this or send multiple arguments back
+        return _borrowRequestList;
+    }
+    
+    function getLendList() public constant returns (request[]) {
+        // send this or send multiple arguments back
+        return _lendRequestList;
+    }
+    */
 }
+
